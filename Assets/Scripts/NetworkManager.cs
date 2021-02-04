2045f9163a69cd99b48aa24ee4fcbacd71aca0c3
Reuben <remcqueen1999@gmail.com>
2021-02-04 01:24:58 +0000

    initial commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+[System.Serializable]
+public class DefaultRoom
+{
+    public string Name;
+    public int sceneIndex;
+    public int maxPlayer;
+}
+
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public List<DefaultRoom> defaultRooms;
+    public GameObject roomUI;
+
+    public void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Trying To Connect To Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server.");
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("Joined the Lobby");
+        roomUI.SetActive(true);
+    }
+
+    public void InitializeRoom(int defaultRoomIndex)
+    {
+        DefaultRoom roomSettings = defaultRooms[defaultRoomIndex];
+
+        //LOAD SCENE
+        PhotonNetwork.LoadLevel(roomSettings.sceneIndex);
+
+        // CREATE THE ROOM
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)roomSettings.maxPlayer;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom(roomSettings.Name, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
